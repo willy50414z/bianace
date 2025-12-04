2d78118276bed67690c3362c52b3d47966e30300
willy50414z <willy50414z@gmail.com>
2025-12-05 01:17:25 +0800

    add build doc

--- build/build_aws_layer.bat
+++ build/build_aws_layer.bat
@@ -1,10 +1,18 @@
 cd ..
-mkdir target/python
+RD /S /Q "target"
 robocopy com target/python /MIR
-conda activate binance
-pip freeze > target/python/requirements.txt
+
+xcopy "build\requirements.txt" "target\python" /E /I /Y
 cd target/python
 pip install --target . -r requirements.txt
 cd ..
-PowerShell -NoProfile -Command "Compress-Archive -Path '.\python' -DestinationPath 'python.zip'"
-xcopy "libs\" "." /E /I /Y
+
+pip install --target . -r requirements.txt
+
+dnf update -y
+dnf install zip -y
+
+cd ..
+zip -r /var/task/python.zip .
+
+docker cp naughty_nightingale:/var/task/python.zip ./
