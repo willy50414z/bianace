fbdec59136e7bcb3896de37e3a9dc5b649da579e
willy50414z <willy50414z@gmail.com>
2025-12-06 00:28:17 +0800

    update build bat

--- build/build_aws_lambdas_image.bat
+++ build/build_aws_lambdas_image.bat
@@ -1,12 +1,11 @@
-pip freeze > requirements.txt
 cd ..
 RD /S /Q "target"
 robocopy com target/com /MIR
 
-xcopy "build\requirements.txt" "target" /E /I /Y
 xcopy "build\Dockerfile" "target" /E /I /Y
 
 cd target
+pip freeze > requirements.txt
 docker build -t ma_7_25_break .
 docker container rm -f ma_7_25_break
 docker run --name ma_7_25_break -p 9000:8080 -d ma_7_25_break
@@ -14,5 +13,16 @@ docker run --name ma_7_25_break -p 9000:8080 -d ma_7_25_break
 cd ..
 RD /S /Q "target"
 
-docker tag ma_7_25_break 449068335280.dkr.ecr.us-east-1.amazonaws.com/binance_trade_bot/ma_7_25_break:latest
-docker push 449068335280.dkr.ecr.us-east-1.amazonaws.com/binance_trade_bot/ma_7_25_break:latest
+docker tag ma_7_25_break 449068335280.dkr.ecr.ap-southeast-1.amazonaws.com/binance_trade_bot/ma_7_25_break:latest
+docker push 449068335280.dkr.ecr.ap-southeast-1.amazonaws.com/binance_trade_bot/ma_7_25_break:latest
+
+@REM us-east-1
+@REM ap-southeast-1
+@REM
+@REM aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 449068335280.dkr.ecr.ap-southeast-1.amazonaws.com
+@REM
+@REM docker tag aa683bd28809 449068335280.dkr.ecr.us-east-1.amazonaws.com/binance_trade_bot/ma_7_25_break:latest
+@REM
+@REM docker push 449068335280.dkr.ecr.us-east-1.amazonaws.com/binance_trade_bot/ma_7_25_break:latest
+@REM
+@REM aws events put-rule --schedule-expression "cron(0,15,30,45 * * * ? *)" --name ma_7_25_break_rule --region ap-southeast-1
